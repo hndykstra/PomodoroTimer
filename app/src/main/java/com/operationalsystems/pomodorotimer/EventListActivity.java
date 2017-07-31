94085ad78ea68595ae91cb7ce2544e5c3d99eb06
Hans Dykstra <hans.dykstra@opsysinc.com>
2017-07-31 13:34:32 -0600

    Begin adding Firebase auth

--- app/src/main/java/com/operationalsystems/pomodorotimer/EventListActivity.java
+++ app/src/main/java/com/operationalsystems/pomodorotimer/EventListActivity.java
@@ -1,18 +1,15 @@
 package com.operationalsystems.pomodorotimer;
 
-import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
-import android.media.audiofx.BassBoost;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.LoaderManager;
-import android.support.v4.content.AsyncTaskLoader;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
@@ -24,13 +21,16 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.operationalsystems.pomodorotimer.data.Event;
 import com.operationalsystems.pomodorotimer.data.Pomodoro;
 import com.operationalsystems.pomodorotimer.data.PomodoroEventContract;
 
 import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -43,10 +43,34 @@ public class EventListActivity extends AppCompatActivity implements LoaderManage
     public static final String PLACEHOLDER_TEAM = "test.operationalsystems";
     public static final String EXTRA_EVENT_ID = "SelectedEventId";
 
+    private class AuthListener implements FirebaseAuth.AuthStateListener {
+
+        @Override
+        public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+            FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+            if (currentUser != null) {
+                theUser = currentUser;
+            } else {
+                startActivityForResult(
+                        AuthUI.getInstance()
+                                .createSignInIntentBuilder()
+                                .setAvailableProviders(
+                                        Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                                new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                                .build(),
+                        RESULT_AUTH_ID);            }
+        }
+    }
+
     private static final String LOG_TAG = "EventListActivity";
     private static final int EVENT_LOADER_ID = 9034;
+    private static final int RESULT_AUTH_ID = 20532;
     private static final String EVENT_LIST_SORT = PomodoroEventContract.Event.ACTIVE_COL + " DESC, "
             + PomodoroEventContract.Event.START_DT_COL + " DESC";
+
+    private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener authListener;
+    private FirebaseUser theUser;
 
     private RecyclerView recycler;
     private EventListAdapter adapter;
@@ -59,6 +83,8 @@ public class EventListActivity extends AppCompatActivity implements LoaderManage
         setSupportActionBar(toolbar);
 
         PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+        auth = FirebaseAuth.getInstance();
+        authListener = new AuthListener();
 
         recycler = (RecyclerView) findViewById(R.id.recycler_events);
         RecyclerView.LayoutManager lm = new GridLayoutManager(this, 1);
@@ -87,6 +113,17 @@ public class EventListActivity extends AppCompatActivity implements LoaderManage
         }
     }
 
+    @Override
+    protected void onResume() {
+        auth.addAuthStateListener(authListener);
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        auth.removeAuthStateListener(authListener);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
