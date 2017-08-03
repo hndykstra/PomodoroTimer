b814f28a3f04dc578dcb4b47f6fb4ee9ad0f8f84
hans.dykstra <hndykstra@gmail.com>
2017-08-02 19:48:50 -0600

    Initial work on Firebase database.

--- app/src/main/java/com/operationalsystems/pomodorotimer/data/PomodoroFirebaseHelper.java
+++ app/src/main/java/com/operationalsystems/pomodorotimer/data/PomodoroFirebaseHelper.java
@@ -22,7 +22,7 @@ public class PomodoroFirebaseHelper {
     /**
      * Queries the database for a user ID. The receiver will get a single callback with the snapshot
      * value when available.
-     * @param userId
+     * @param userId Uid of the user to fetch
      * @param valueReceiver Single value listener receives a callback
      */
     public void queryUser(String userId, ValueEventListener valueReceiver) {
@@ -41,7 +41,7 @@ public class PomodoroFirebaseHelper {
     /**
      * Adds a listener that receives child events for teams. This will receive childAdded events for existing teams
      * and other events as data changes, until unsubscribed.
-     * @param teamReceiver
+     * @param teamReceiver Callback will receive data notifications.
      */
     public void subscribePublicTeams(final ChildEventListener teamReceiver) {
         PomodoroFirebaseContract.getTeamsReference(database)
@@ -52,7 +52,10 @@ public class PomodoroFirebaseHelper {
 
     public void unsubscribePublicTeams(final ChildEventListener teamReceiver) {
         // TODO: does this work, or do I need to keep a reference to the original Query?
+        // it appears the listener is added / removed to some underlying DB resource
+        // so removing it this way will work... maybe
         PomodoroFirebaseContract.getTeamsReference(database)
                 .removeEventListener(teamReceiver);
     }
+
 }
