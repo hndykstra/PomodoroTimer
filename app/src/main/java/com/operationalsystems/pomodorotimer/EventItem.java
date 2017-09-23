c751bb8707356c2de8dadb43c81dd5bed1abda54
Hans Dykstra <hans.dykstra@opsysinc.com>
2017-09-23 08:00:57 -0600

    fix gradle

--- app/src/main/java/com/operationalsystems/pomodorotimer/EventItem.java
+++ app/src/main/java/com/operationalsystems/pomodorotimer/EventItem.java
@@ -10,6 +10,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
+ * Just a view holder for the event list.
  * Created by Hans on 9/22/2017.
  */
 public class EventItem extends RecyclerView.ViewHolder {
@@ -24,6 +25,10 @@ public class EventItem extends RecyclerView.ViewHolder {
         ButterKnife.bind(this, itemView);
     }
 
+    /**
+     * Binds data in this holder to the event object. Package access.
+     * @param event
+     */
     void bind(Event event) {
         this.boundEvent = event;
         titleView.setText(event.getName());
